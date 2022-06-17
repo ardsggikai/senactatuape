745e3e3bd2016d73e53afb8ecbc081415224b431
Állan Richard De Souza Gomes <88465304+ardsggikai@users.noreply.github.com>
2022-06-17 18:27:45 -0300

    Add files via upload

---
+++ senactat-aulas-ti/UC9/Aulas/eclipse-workspace/Fundamentos/src/fundamentos/Hello.java
@@ -0,0 +1,23 @@
+/**
+ * Hello World
+ */
+package fundamentos;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author Allan Gomes
+ *
+ */
+public class Hello {
+
+	/**
+	 * Método principal
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+        JOptionPane.showMessageDialog(null, "Hello World");
+	}
+
+}
