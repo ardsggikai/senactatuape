4baa00a410c077bb6c6a7af4f0d50c575c8e4977
Állan Richard De Souza Gomes <88465304+ardsggikai@users.noreply.github.com>
2022-06-17 18:47:44 -0300

    Add files via upload

---
+++ senactat-aulas-ti/UC9/Aulas/eclipse-workspace/Fundamentos/src/fundamentos/Variaveis.java
@@ -0,0 +1,24 @@
+/**
+ * Estudo das variáveis
+ */
+package fundamentos;
+
+/**
+ * @author Allan Gomes
+ *
+ */
+public class Variaveis {
+
+	/**
+	 * Método principal
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Criando (declarando) uma variavel no Java
+        String nome;
+        nome = "Allan Gomes";
+        System.out.println("Nome: " + nome);
+        
+	}
+
+}
---
+++ senactat-aulas-ti/UC9/Aulas/eclipse-workspace/Portfólio de Projetos/src/portfolio/Sobre.java
@@ -0,0 +1,47 @@
+package portfolio;
+
+import java.awt.EventQueue;
+
+import javax.swing.JDialog;
+import javax.swing.JTextPane;
+
+public class Sobre extends JDialog {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Sobre dialog = new Sobre();
+					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					dialog.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public Sobre() {
+		setTitle("Sobre");
+		setModal(true);
+		setBounds(100, 100, 500, 300);
+		getContentPane().setLayout(null);
+		
+		JTextPane txtpnEmDesenvolvimento = new JTextPane();
+		txtpnEmDesenvolvimento.setText("EM DESENVOLVIMENTO");
+		txtpnEmDesenvolvimento.setBounds(20, 11, 418, 225);
+		getContentPane().add(txtpnEmDesenvolvimento);
+
+	}
+}
